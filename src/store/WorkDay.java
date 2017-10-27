8e5f949f55ade396c11185e395464078557167de
ukhanyov <ukhanyov.oleg@gmail.com>
2017-10-27 17:36:24 +0300

    Implemented a restock functionality in WorkDay class

--- src/store/WorkDay.java
+++ src/store/WorkDay.java
@@ -38,8 +38,15 @@ public class WorkDay {
     }
 
     private void checkIfTheRefillOfTheStockIsNeeded() {
-        //TODO:
-        //Create a functionality for refilling stores stock
+
+        for(AcquisitionUnit unit : listOfAllDrinks){
+            if(unit.getStock() < 10){
+                unit.setStock(unit.getStock() + 150);
+                cleanProfitOfTheDay = cleanProfitOfTheDay - (unit.getPurchase_price()*150);
+                System.out.println("Bought 150 unit of: " + unit.getName().toString());
+            }
+        }
+        System.out.println("Clean profit of the day after restock: " + Float.toString(cleanProfitOfTheDay));
     }
 
     private void generateOrderForTheNextHour() {
@@ -83,7 +90,7 @@ public class WorkDay {
         //Implement the rewriting of the stock file
 
         if(listOfOrderedDrinks.isEmpty()){
-            System.out.println("Customer chose nothing<-------------------------------------------------------------");
+            System.out.println("Customer chose nothing<-------------------------------------------------------------------------------------------------------------");
         }else {
             int counter = 1; //Amount of drinks in the list of a customer
 
@@ -93,26 +100,7 @@ public class WorkDay {
                 System.out.printf("%-5s", Float.toString(unit.getPurchase_price()));
                 System.out.print(" *** ");
 
-                if(counter > 2){
-                    cleanProfitOfTheDay += unit.getPurchase_price()*0.07;
-                    System.out.print(" 7%  *** ");
-                    System.out.printf("%-2s", Float.toString(unit.getPurchase_priceWithExtraCharge(0.07f)));
-                }else{
-                    if(CURRENT_TIME.plusMinutes(1).isAfter(TIME_DISCOUNT_START) && CURRENT_TIME.plusMinutes(1).isBefore(TIME_DISCOUNT_END)){
-                        cleanProfitOfTheDay += unit.getPurchase_price()*0.08;
-                        System.out.print(" 8%  *** ");
-                        System.out.printf("%-2s",Float.toString(unit.getPurchase_priceWithExtraCharge(0.08f)));
-                    }else{ if(isWeekend){
-                                cleanProfitOfTheDay += unit.getPurchase_price()*0.15;
-                                System.out.print(" 15% *** ");
-                                System.out.printf("%-2s",Float.toString(unit.getPurchase_priceWithExtraCharge(0.15f)));
-                            }else{
-                                    cleanProfitOfTheDay += unit.getPurchase_price()*0.1;
-                                    System.out.print(" 10% *** ");
-                                    System.out.printf("%-2s",Float.toString(unit.getPurchase_priceWithExtraCharge(0.1f)));
-                                }
-                    }
-                }
+                extraChargeCalculations(unit, counter);
 
                 System.out.print("\n");
 
@@ -125,6 +113,32 @@ public class WorkDay {
         }
     }
 
+    private void extraChargeCalculations(AcquisitionUnit unit, int counter){
+        if(counter > 2){
+            cleanProfitOfTheDay += unit.getPurchase_price()*0.07;
+            System.out.print(" 7%  *** ");
+            System.out.printf("%7.6s", Float.toString(unit.getPurchase_priceWithExtraCharge(0.07f)));
+            System.out.print(" *** Rule of extra charge: More the 2 items on the shopping list");
+        }else{
+            if(CURRENT_TIME.plusMinutes(1).isAfter(TIME_DISCOUNT_START) && CURRENT_TIME.plusMinutes(1).isBefore(TIME_DISCOUNT_END)){
+                cleanProfitOfTheDay += unit.getPurchase_price()*0.08;
+                System.out.print(" 8%  *** ");
+                System.out.printf("%7.6s",Float.toString(unit.getPurchase_priceWithExtraCharge(0.08f)));
+                System.out.print(" *** Rule of extra charge: Discount hours (from 18:00 till 20:00)");
+            }else{ if(isWeekend){
+                cleanProfitOfTheDay += unit.getPurchase_price()*0.15;
+                System.out.print(" 15% *** ");
+                System.out.printf("%7.6s",Float.toString(unit.getPurchase_priceWithExtraCharge(0.15f)));
+                System.out.print(" *** Rule of extra charge: Currently is a weekend day");
+            }else{
+                cleanProfitOfTheDay += unit.getPurchase_price()*0.1;
+                System.out.print(" 10% *** ");
+                System.out.printf("%7.6s",Float.toString(unit.getPurchase_priceWithExtraCharge(0.1f)));
+                System.out.print(" *** Rule of extra charge: Default extra charge");
+            }
+            }
+        }
+    }
 
     private void reduceStock(AcquisitionUnit unit) {
         for (AcquisitionUnit unit_from_main_list : listOfAllDrinks){
@@ -138,14 +152,14 @@ public class WorkDay {
     private void generateActionsForAnHour(){
         int number_of_customers_in_next_hour = new Random().nextInt(10) + 1;
 
-        System.out.println("////////////////////////////////////////////////");
+        System.out.println("////////////////////////////////////////////////////////////////////////////////////////////////");
         System.out.println("Customers from the time interval from: " + CURRENT_TIME.toString() + " to " + CURRENT_TIME.plusHours(1).toString());
-        System.out.println("////////////////////////////////////////////////");
+        System.out.println("////////////////////////////////////////////////////////////////////////////////////////////////");
 
         for(int i = 0; i < number_of_customers_in_next_hour; i++){
             generateOrderForTheNextHour();
             executeOrder();
-            System.out.println("------------------------------------------------");
+            System.out.println("------------------------------------------------------------------------------------------------");
         }
 
         CURRENT_TIME = CURRENT_TIME.plusHours(1);
